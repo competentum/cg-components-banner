edb68610829d4a690de47ea6a220ae69feca4437
Alexey Pilyugin <pilyugin@competentum.ru>
2016-04-07 18:17:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var startYear = 2015;
+var currentYear = (new Date()).getFullYear();
+
+var banner = '(c) ' + startYear + '-' + currentYear + ' Competentum Group | http://competentum.com';
+
+module.exports = banner;
