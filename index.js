1a23d57a6d3f25aaeb49f9c9a1575b47a7b11cb6
Alexey Pilyugin <pilyugin@competentum.ru>
2016-07-14 10:06:04 +0300

    Update README.md. Added MIT license.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var startYear = 2015;
 var currentYear = (new Date()).getFullYear();
 
-var banner = '(c) ' + startYear + '-' + currentYear + ' Competentum Group | http://competentum.com';
+var banner = '(c) ' + startYear + '-' + currentYear + ' Competentum Group | http://competentum.com\n'
+             + 'Released under the MIT license\n'
+             + 'https://opensource.org/licenses/mit-license.php';
 
 module.exports = banner;
